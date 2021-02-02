c0dc79ddc2db444b7e6fe6ebf295a5329a09e0e5
SZigmund <szigmund@hotmail.com>
2021-02-01 19:25:07 -0500

    Update AutoWoot.js

--- AutoWoot.js
+++ AutoWoot.js
@@ -1,4 +1,15 @@
+//javascript:(function(){$.getScript('');}());
+
 //SECTION MyEVENTS: Events call from host:
+var MyVARS = {
+	windowTitle: ""
+}
+var API = {
+  SongUpVote: function() {
+    try			{ document.querySelector("#currSongThumbUp_forTutorial").click();    } 
+	catch (err) { console.log("connectAPI: " + err.message); }
+  }
+}
 var MyEVENTS = {
   connectAPI: function() {
     try{
@@ -8,15 +19,12 @@ var MyEVENTS = {
 		if (docEl && docEl.addEventListener) {
 			docEl.addEventListener("DOMSubtreeModified", function(evt) {
 				var t = evt.target;
-				if (t === titleEl || (t.parentNode && t.parentNode === titleEl)) {
-					MyEVENTS.eventDjadvance();
-				}
+				if (t === titleEl || (t.parentNode && t.parentNode === titleEl)) { MyEVENTS.eventDjadvance(); }
 			}, false);
 		} else {
 			document.onpropertychange = function() {
-				if (window.event.propertyName == "title") {
-					MyEVENTS.eventDjadvance();
-				}
+				if (window.event.propertyName == "title") { MyEVENTS.eventDjadvance(); }
+				if (window.event.propertyName !== "title") { MyEVENTS.eventLog(evt); }
 			};
 		}
 	  
@@ -24,14 +32,23 @@ var MyEVENTS = {
       console.log("connectAPI: " + err.message);
     }
   },
-  eventDjadvance: function(obj) {
+  eventDjadvance: function() {
     try{
-	      console.log("Title modifed: " + );
+		if (MyVARS.windowTitle !== document.getElementsByTagName("title")[0].text && 
+		    document.getElementsByTagName("title")[0].text.length > 1) {
+	      console.log("Title modifed: " + document.getElementsByTagName("title")[0].text + " > " + document.getElementsByTagName("title")[0].text.length.toString());
+		  MyVARS.windowTitle = document.getElementsByTagName("title")[0].text;
+		  setTimeout(function() { API.SongUpVote(); }, 1500);
+	    }
 	  }
-    } catch (err) {
-      console.log("eventDjadvance: " + err.message);
-    }
-  }
+    catch (err) {	console.log("eventDjadvance: " + err.message); }
+  },
+  eventLog: function(evt) {
+    try{
+	 console.log("EVENT: " + evt.target);
+	}
+    catch (err) {	console.log("eventDjadvance: " + err.message); }
+  },
 };
 
 var STARTUP = {
